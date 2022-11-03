dc2ce45c7f0893118006953c13260f34d2f8219e
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-11-03 10:49:23 +0530

    add assignment

---
+++ Assignment/DharaniVijay/Assignment_2/Flask_With_DB2/Connection/Connect.py
@@ -0,0 +1,44 @@
+import ibm_db
+
+
+def Connection():
+    try:
+        conn = ibm_db.connect(
+            "DATABASE=bludb;HOSTNAME=9938aec0-8105-433e-8bf9-0fbb7e483086.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32459;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=nwc93718;PWD=x8XQTQh8p0RAm8VM", "", "")
+        print("Database Connected Successfully !")
+        return conn
+    except:
+        print("Unable to connect: ", ibm_db.conn_errormsg())
+
+
+def Create(email, name, phone, password, conn):
+
+    columns = '"UNAME","UEMAIL","UPHONE","UPASSWORD"'
+    val = "'"+name+"','"+email+"','"+phone+"','"+password+"'"
+    sql = 'Insert into NWC93718.USER(' + columns + ') values('+val+')'
+    try:
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.execute(stmt)
+        print("added :-)")
+        return 1
+    except:
+        print("Error While Adding the User ! ")
+        return 0
+
+
+def Signin(email, password, conn):
+
+    sql = "SELECT * FROM NWC93718.USER"
+    try:
+        result = ibm_db.exec_immediate(conn, sql)
+        tuple = ibm_db.fetch_tuple(result)
+        while tuple != False:
+            if str(tuple[1]) == email and str(tuple[3]) == password:
+                res = [str(tuple[0]), str(tuple[1]), str(tuple[2])]
+                return res
+            tuple = ibm_db.fetch_tuple(result)
+        print("Fetch Success :-)")
+        return 0
+    except:
+        print("fetch not found !")
+        return 0
