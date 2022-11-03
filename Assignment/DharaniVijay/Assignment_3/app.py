2e9a7e6dfea426aa9dde5b3d42dcb02358e9d6d1
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-11-03 21:27:13 +0530

    add assignment

--- Assignment/DharaniVijay/Assignment_3/app.py
+++ Assignment/DharaniVijay/Assignment_3/app.py
@@ -1,18 +1,21 @@
-from flask import Flask,redirect, render_template, request
+from flask import Flask, redirect, render_template, request
 import Connection
- 
+
 app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
 
 conn = Connection.Connect()
-files = Connection.get_bucket_contents("nalaiyathiransample",conn)
+files = Connection.get_bucket_contents("ntas3", conn)
 print(files)
 
-#Index page
+# Index page
+
+
 @app.route('/',)
 def index():
-    return render_template('index.html',files = files,len=len(files))
+    return render_template('index.html', files=files, len=len(files))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # Connection.get_bucket_contents("nalaiyathiransample",conn)
     # Connection.get_buckets(conn)
-    app.run(debug = True)
+    app.run(debug=True)
